aee2930dda8457eadc3f356ca2f8ce90a9d88d24
ubaid ur rehman <programming112233@gmail.com>
2024-11-20 15:49:48 +0500

    put

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-import { Nunito } from "next/font/google";  // Import Nunito from Google Fonts
+import { Nunito } from "next/font/google"; // Import Nunito from Google Fonts
 import "./globals.css";
 
 const nunito = Nunito({
   subsets: ["latin"],
   variable: "--font-nunito",
-  weight: ["400", "700"],  
+  weight: ["400", "700"],
 });
 
 export const metadata = {
@@ -15,11 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${nunito.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <meta
+          name="simpledcver"
+          content="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJtb3dhcGF5LmNvbSIsImV4cCI6MTczMjY2NTYwMH0.ZH2AmfcP3jqP3SW-heJ6oSJgYE4gvfCqApWoPI_Q0JM"
+        />
+      </head>
+      <body className={`${nunito.variable} antialiased`}>{children}</body>
     </html>
   );
 }
